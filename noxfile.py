5317753d8e6646291d8bbc01d9876aad82c2e63c
Diego Ramirez <dr01191115@gmail.com>
2022-01-07 12:13:18 -0600

    Add CI (#12)
    * Separate linters and formatters in the noxfile
    
    * Add GHA stuff

--- noxfile.py
+++ noxfile.py
@@ -3,16 +3,22 @@ I use Nox here to reformat the code.
 """
 import nox
 
+deps = ["black==21.11b1", "flake8==4.0.1", "isort==5.10.1"]
+files = ["noxfile.py", "main.py"]
+
 
 @nox.session(name="keep-codebase-clean")
 def keep_codebase_clean(session):
-    "Run linters and formatters."
-    files = ("noxfile.py", "main.py")
-    session.install("black==21.11b1", "flake8==4.0.1", "isort==5.10.1")
-    # format
+    "Run formatters."
+    session.install(*deps)
     session.run("isort", *files)
     session.run("black", *files)
-    # lint
+
+
+@nox.session(name="check-quality")
+def check_quality(session):
+    "Check the style and quality."
+    session.install(*deps)
     session.run("flake8", *files, "--max-line-length=127")
     session.run("isort", "--check-only", *files)
     session.run("black", "--check", *files)
