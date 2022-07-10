DiddiLeija/diddi-and-the-bugs

commit aaec68e4c16e1a272be386147da993bb1a1fbdc6

Notify if the Windows executable build is skipped
It's polite to notify when a step is ignored... well, that's what I think.

noxfile.py
@@ -95,6 +95,9 @@ def create_packages(session):
             "shutil.copy2('resource.pyxres', f'./build/{exe_path}/resource.pyxres')",
         )
         session.run("python", "-m", "zipfile", "-c", "./dist/windows.zip", "./build")
+    else:
+        # It's polite to notify when a step is ignored... well, that's what I think.
+        session.warn("Not running in Windows, ignoring the cx_Freeze executable build...")
     # Before closing, it's cleanup time!
     session.warn("Cleaning up the excedents...")
     session.run(

commit a1d2ca898f346c4731093711ada289528dcf70e0

Fix the code style, and the issue introduced by aaec68e

noxfile.py
@@ -97,7 +97,9 @@ def create_packages(session):
         session.run("python", "-m", "zipfile", "-c", "./dist/windows.zip", "./build")
     else:
         # It's polite to notify when a step is ignored... well, that's what I think.
-        session.warn("Not running in Windows, ignoring the cx_Freeze executable build...")
+        session.warn(
+            "Not running in Windows, ignoring the cx_Freeze executable build..."
+        )
     # Before closing, it's cleanup time!
     session.warn("Cleaning up the excedents...")
     session.run(